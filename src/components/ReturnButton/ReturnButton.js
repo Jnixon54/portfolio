dfc1ed391d6ffedc363f8df843013be31b5e0c3f
Jnixon54 <Jnixon54@gmail.com>
2017-12-29 21:42:13 -0600

    Anim: Return button changed to pure CSS

--- src/components/ReturnButton/ReturnButton.js
+++ src/components/ReturnButton/ReturnButton.js
@@ -2,15 +2,20 @@ import React, { Component } from 'react';
 import './ReturnButton.css';
 import {Link} from 'react-router-dom';
 
+import chevronUp from '../../icons/chevron_up.svg';
+
 
 class ReturnButton extends Component {
+  
   render() {
+    console.log(this.props.scrollPercent)
+    let classes = '';
+    classes = this.props.scrollPercent > 22 ? 'active' : 'hidden';
     return (
-      <div className="return-button-container">
+      <div className={'return-button-container ' + classes}>
       <Link onClick={() => this.props.handleScroll('header')} to="#home">
         <div className="return-button">
-          <div />
-          <div />
+          <img src={chevronUp} className="chevron-up" alt="" />
         </div>
       </Link>
       </div>
